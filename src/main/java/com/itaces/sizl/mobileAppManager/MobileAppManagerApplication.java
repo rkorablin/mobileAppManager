0e04fa0922b1a3e51a0096a38f9a1c53f474ec79
rkorablin <rkorablin-nk@yandex.ru>
2024-10-02 05:20:22 +0300

    first commit

---
+++ src/main/java/com/itaces/sizl/mobileAppManager/MobileAppManagerApplication.java
@@ -0,0 +1,13 @@
+package com.itaces.sizl.mobileAppManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itaces/sizl/mobileAppManager/MobileAppManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itaces.sizl.mobileAppManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
